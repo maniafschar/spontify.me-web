db6a09b2b3bb78ebaf79c93bc8d6c355b3b9dde3
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2022-10-04 08:02:08 +0200

    0.2.3

--- src/js/global.js
+++ src/js/global.js
@@ -8,7 +8,7 @@ export { global };
 
 class global {
 	static appTitle = 'spontify.me';
-	static appVersion = '0.2.2';
+	static appVersion = '0.2.3';
 	static language = null;
 	static minLocations = 5;
 	static paused = false;
