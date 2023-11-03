621fceda54af3ee2a8134e930ecb1cb5bfb49827
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2023-11-03 09:52:01 +0100

    Update InputDate.js

--- src/js/customElements/InputDate.js
+++ src/js/customElements/InputDate.js
@@ -166,7 +166,7 @@ label.filled {
 		}
 		this.setAttribute('value', s);
 		this.setAttribute('complete', '' + (s.length == 10 || s.length == 19));
-		this.dispatchEvent(new CustomEvent('Date', { detail: { id: this.x, value: s } }));
+		this.dispatchEvent(new CustomEvent('Date', { detail: { id: this.x, value: s, complete: this.getAttribute('complete') } }));
 	}
 	toggle(e, html) {
 		var m = parseInt(ui.cssValue('dialog-hint', 'margin-top'));
