683d77085e65d12c63baa2ae5b417102a4a7495c
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-01-11 22:54:57 +0100

    npe in home

--- src/js/pageHome.js
+++ src/js/pageHome.js
@@ -216,8 +216,7 @@ border-radius: 0.5em 0 0 3em;
 						setTimeout(f, 100);
 				}
 				f();
-				if (ui.q('home text.town'))
-					ui.q('home text.town').style.display = 'none';
+				ui.css('home text.town', 'display', 'none');
 			}
 		}
 		pageHome.initNotificationButton();
