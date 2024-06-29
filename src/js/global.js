56f485c72cfca095fba1642c7cbd651b1c6386ed
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-06-29 11:30:40 +0200

    0.6.9

--- src/js/global.js
+++ src/js/global.js
@@ -9,7 +9,7 @@ export { Strings, global };
 
 class global {
 	static appTitle = '{placeholderAppTitle}';
-	static appVersion = '0.6.8';
+	static appVersion = '0.6.9';
 	static config = JSON.parse('{placeholderAppConfig}');
 	static imprintCustom = '{imprintCustom}';
 	static language = null;
