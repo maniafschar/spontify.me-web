7a30bc0e90d9113cf7ee31a95cf877498b7e4343
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-09-01 20:46:53 +0200

    marketing create location user

--- src/js/init.js
+++ src/js/init.js
@@ -46,16 +46,10 @@ class initialisation {
 		tag = tag.split('&');
 		communication.ajax({
 			url: global.serverApi + 'marketing/user/' + tag[1].substring(2) + '/' + tag[2].substring(2),
-			webCall: 'init.init',
+			webCall: 'init.createLocationUser',
 			method: 'POST',
 			success(r) {
-				if (r == 'noError')
-					;
-				else if (r == 'errorContactId')
-					;
-				else if (r == 'errorHash')
-					;
-				ui.navigation.openHint({ desc: '<div style="width:100%;max-height:80vh;overflow-y:auto;">' + ui.l('home.marketingCreateLocationUser') + '<br/><br/><button-text onclick="initialisation.createLocationUser(&quot;' + p + '&quot;)" label="home.marketingCreateLocationUserButton"></button-text></div>', pos: '5%,2em', size: '90%,auto', onclick: 'return false;' });
+				ui.navigation.openHint({ desc: ui.l('home.marketingCreateLocationUserResult'), pos: '5%,2em', size: '90%,auto', onclick: 'return false;' });
 			}
 		});
 	}
@@ -435,9 +429,17 @@ class initialisation {
 			setTimeout(function () {
 				if (ui.q('head link[rel="canonical"]').getAttribute('href').split('/').length > 4)
 					ui.navigation.openHint({ desc: '<div style="width:100%;max-height:80vh;overflow-y:auto;"><img src="images/celebrate.svg" style="max-width:70%;"/><br/>' + ui.l('home.marketingOpenArticleText') + '<br/><br/><button-text onclick="ui.navigation.autoOpen(&quot;' + p + '&quot;)" label="home.marketingOpenArticleButton"></button-text></div>', pos: '5%,2em', size: '90%,auto', onclick: 'return false;' });
-				else if (p.indexOf('c=') == 0 && p.indexOf('&i=') > 0 && p.indexOf('&h=') > 0)
-					ui.navigation.openHint({ desc: ui.l('home.marketingCreateLocationUser') + '<br/><br/><button-text onclick="initialisation.createLocationUser(&quot;' + p + '&quot;)" label="home.marketingCreateLocationUserButton"></button-text>', pos: '5%,2em', size: '90%,auto', onclick: 'return false;' });
-				else
+				else if (p.indexOf('c=') == 0 && p.indexOf('&i=') > 0 && p.indexOf('&h=') > 0) {
+					p = p.split('&');
+					communication.ajax({
+						url: global.serverApi + 'marketing/user/' + p[1].substring(2) + '/' + p[2].substring(2),
+						webCall: 'init.showStartDialogs',
+						success(r) {
+							if (r == 'true')
+								ui.navigation.openHint({ desc: ui.l('home.marketingCreateLocationUser') + '<br/><br/><button-text onclick="initialisation.createLocationUser(&quot;' + p + '&quot;)" label="home.marketingCreateLocationUserButton"></button-text>', pos: '5%,2em', size: '90%,auto', onclick: 'return false;' });
+						}
+					});
+				} else
 					ui.navigation.autoOpen(p);
 			}, 2500);
 		} else
