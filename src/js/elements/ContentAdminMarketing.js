283b64becff4d804b7b032354495582f85324141
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-10-01 10:35:18 +0200

    Update ContentAdminMarketing.js

--- src/js/elements/ContentAdminMarketing.js
+++ src/js/elements/ContentAdminMarketing.js
@@ -546,7 +546,7 @@ results freetext div {
 	save() {
 		var o = ContentAdminMarketing.html2json();
 		communication.ajax({
-			url: global.serverApi + 'db/one',
+			url: global.serverApi + 'db/one' + (o.id ? '/' + o.id : '),
 			method: o.id ? 'PUT' : 'POST',
 			body: { classname: 'ClientMarketing', id: o.id, values: o },
 			responseType: 'json',
