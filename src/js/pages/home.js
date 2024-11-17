40a7f85ef55b69a51952a4fb3220dd6999f2f7ea
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-11-17 11:24:54 +0100

    cosmetics

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -402,7 +402,7 @@ skills {
 						return;
 					}
 					if (v.id == user.clientId)
-						v.image = 'source="admin" class="admin mainBG"';
+						v.image = 'source="admin" class="admin"';
 					else if (v.imageList)
 						v.image = 'src="' + global.serverImg + v.imageList + '"';
 					else if (v.contactNotification.action.indexOf('news=') == 0 || v.contactNotification.action.indexOf('m=') == 0)
