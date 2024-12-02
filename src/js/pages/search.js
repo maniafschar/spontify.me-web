45439b6d12f691b2b7402d6294895496b1db6beb
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-12-02 10:08:52 +0100

    Update search.js

--- src/js/pages/search.js
+++ src/js/pages/search.js
@@ -442,8 +442,8 @@ ${v.keywords}
 			var d = JSON.parse(decodeURIComponent(rows[i].getAttribute('data')));
 			if (d.latitude == event.latLng.lat() && d.longitude == event.latLng.lng()) {
 				ui.classAdd(rows[i], 'highlightMap');
-				for (var i = 0; i < pageSearch.map.markerLocation.length; i++)
-					pageSearch.map.markerLocation[i].setOpacity(pageSearch.map.markerLocation[i].id == rows[i].getAttribute('i') ? 1 : 0.2);
+				for (var i2 = 0; i2 < pageSearch.map.markerLocation.length; i2++)
+					pageSearch.map.markerLocation[i2].setOpacity(pageSearch.map.markerLocation[i2].id == rows[i].getAttribute('i') ? 1 : 0.2);
 				break;
 			}
 		}
